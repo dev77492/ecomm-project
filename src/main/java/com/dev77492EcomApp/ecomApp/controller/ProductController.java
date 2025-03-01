96380aa30b2c6fb3205d70a1cb9c91a15bd3f75e
dev77492 <aniketsawant94053@gmail.com>
2025-03-01 14:50:58 +0530

    update and delete methods added

--- src/main/java/com/dev77492EcomApp/ecomApp/controller/ProductController.java
+++ src/main/java/com/dev77492EcomApp/ecomApp/controller/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.swing.plaf.InsetsUIResource;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -58,5 +60,39 @@ public class ProductController {
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+    }
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) {
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to Update" , HttpStatus.BAD_REQUEST);
+        }
+
+        if(product1 != null){
+            return new ResponseEntity<>("Updated" , HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Failed to Update" , HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product !=null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted ", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Product Not Found", HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword){
+        List<Product> products= service.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
